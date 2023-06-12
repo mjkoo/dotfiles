4d6c123a82b133c6de7a5dae446f1b383905661f
Maxwell Koo <mjkoo90@gmail.com>
2023-06-12 10:35:56 -0400

    New tmux and nvim

---
+++ dot_config/nvim/lua/custom/chadrc.lua
@@ -0,0 +1,24 @@
+---@type ChadrcConfig
+local M = {}
+
+-- Path to overriding theme and highlights files
+local highlights = require "custom.highlights"
+
+M.ui = {
+  theme = "chadracula",
+  theme_toggle = { "chadracula", "one_light" },
+
+  statusline = {
+    theme = "minimal",
+  },
+
+  hl_override = highlights.override,
+  hl_add = highlights.add,
+}
+
+M.plugins = "custom.plugins"
+
+-- check core.mappings for table structure
+M.mappings = require "custom.mappings"
+
+return M
