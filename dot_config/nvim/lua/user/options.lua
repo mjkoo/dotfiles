468637970d8c59393434639dba8be8f5eec48352
Maxwell Koo <mjkoo90@gmail.com>
2023-06-13 18:57:52 -0400

    Firenvim

--- dot_config/nvim/lua/user/options.lua
+++ dot_config/nvim/lua/user/options.lua
@@ -9,14 +9,27 @@ return {
     guifont = "JetBrainsMono Nerd Font:h10",
   },
   g = {
-    mapleader = " ",                 -- sets vim.g.mapleader
-    autoformat_enabled = true,       -- enable or disable auto formatting at start (lsp.formatting.format_on_save must be enabled)
-    cmp_enabled = true,              -- enable completion at start
-    autopairs_enabled = true,        -- enable autopairs at start
-    diagnostics_mode = 3,            -- set the visibility of diagnostics in the UI (0=off, 1=only show in status line, 2=virtual text off, 3=all on)
-    icons_enabled = true,            -- disable icons in the UI (disable if no nerd font is available, requires :PackerSync after changing)
+    mapleader = " ", -- sets vim.g.mapleader
+    autoformat_enabled = true, -- enable or disable auto formatting at start (lsp.formatting.format_on_save must be enabled)
+    cmp_enabled = true, -- enable completion at start
+    autopairs_enabled = true, -- enable autopairs at start
+    diagnostics_mode = 3, -- set the visibility of diagnostics in the UI (0=off, 1=only show in status line, 2=virtual text off, 3=all on)
+    icons_enabled = true, -- disable icons in the UI (disable if no nerd font is available, requires :PackerSync after changing)
     ui_notifications_enabled = true, -- disable notifications when toggling UI elements
-    resession_enabled = false,       -- enable experimental resession.nvim session management (will be default in AstroNvim v4)
+    resession_enabled = false, -- enable experimental resession.nvim session management (will be default in AstroNvim v4)
+    firenvim_config = {
+      globalSettings = {
+        alt = "all",
+        ["<C-w>"] = "noop",
+        ["<C-n>"] = "default",
+      },
+      localSettings = {
+        [".*"] = {
+          cmdline = "neovim",
+          takeover = "never",
+        },
+      },
+    },
   },
 }
 -- If you need more control, you can use the function()...end notation
