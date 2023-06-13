96ffee3fb2a3652c6df5043d3718189ca1b50ded
Maxwell Koo <mjkoo90@gmail.com>
2023-06-13 17:06:28 -0400

    Use packs

--- dot_config/nvim/lua/user/plugins/mason.lua
+++ dot_config/nvim/lua/user/plugins/mason.lua
@@ -3,7 +3,6 @@ return {
     "williamboman/mason-lspconfig.nvim",
     opts = function(_, opts)
       opts.ensure_installed = require("astronvim.utils").list_insert_unique(opts.ensure_installed, {
-        "lua_ls",
       })
     end,
   },
@@ -11,8 +10,6 @@ return {
     "jay-babu/mason-null-ls.nvim",
     opts = function(_, opts)
       opts.ensure_installed = require("astronvim.utils").list_insert_unique(opts.ensure_installed, {
-        "prettier",
-        "stylua",
       })
     end,
   },
@@ -20,7 +17,6 @@ return {
     "jay-babu/mason-nvim-dap.nvim",
     opts = function(_, opts)
       opts.ensure_installed = require("astronvim.utils").list_insert_unique(opts.ensure_installed, {
-        "python",
       })
     end,
   },
